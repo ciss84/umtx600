25c6f3097b425654fd3b12e19c8767d292cc8e47
Mugiwara <37186888+ciss84@users.noreply.github.com>
2024-12-18 23:12:20 +0100

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,13 @@
+        function showMessage(msg) {
+            document.getElementById("message").innerHTML = msg;
+        }
+
+        function allset() {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById("allset").style.display = "block";
+        }
+
+        function elfldrpl() {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById("awaiting").style.display = "block";
+        }
