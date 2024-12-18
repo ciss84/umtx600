6e60dc5a5fb9e03346004ba1666dfef794f37923
Mugiwara <37186888+ciss84@users.noreply.github.com>
2024-12-18 16:01:38 +0100

    Add files via upload

--- psfreec.js
+++ psfreec.js
@@ -464,7 +464,7 @@ async function make_arw(reader, view2, pop) {
 }
 async function run() {
     try{
-    StartTimer();
+    //StartTimer();
     debug_log('STAGE: UaF SSV');
     const [fsets, indices] = prepare_uaf();
     const view = await uaf_ssv(fsets, indices[1]);
