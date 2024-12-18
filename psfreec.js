ef71e512962da4203751c54a7acb33c26356e9e7
Mugiwara <37186888+ciss84@users.noreply.github.com>
2024-12-18 17:20:29 +0100

    Update psfreec.js

--- psfreec.js
+++ psfreec.js
@@ -49,7 +49,7 @@ const [is_ps4, version] = (() => {
     }
     return [is_ps4, version];
 })();
-/*const ssv_len = (() => {
+const ssv_len = (() => {
     if (0x600 <= config.target && config.target < 0x650) {
         return 0x58;
     }
@@ -59,8 +59,8 @@ const [is_ps4, version] = (() => {
     if (0x650 <= config.target && config.target < 0x900) {
         return 0x48;
     }
-})();*/
-const ssv_len = 0x50;
+})();
+//const ssv_len = 0x50;
 const num_fsets = 0x180;
 const num_spaces = 0x40;
 const num_adjs = 6;
