62b5835386a08269d926523a7318c4d5900645d5
Mugiwara <37186888+ciss84@users.noreply.github.com>
2024-12-19 13:41:21 +0100

    Add files via upload

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 function done() {
     document.getElementById("loader").style.display = "none";
-    document.getElementById("allset").style.display = "block";
+    document.getElementById("alldone").style.display = "block";
 }
 function CalcTime(dur){hrs=Math.floor(dur/1000/60/60);min=Math.floor(dur/1000/60-hrs*60);sec=Math.floor(dur/1000-min*60);mil=dur.toString().slice(-3);if (min!=0){ShowDuration=" - WK Exploited In : "+min+" minute"+(min==1?"":"s")+", "+sec+" second"+(sec==1?"":"s");}else {ShowDuration=" - Exploited In: "+sec+" second"+(sec==1?"":"s");}}
 function StartTimer(){StartTime=Date.now();}
