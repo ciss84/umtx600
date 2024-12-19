f5c9f82a2b627d3f209546a177dfb70b209ffd44
Mugiwara <37186888+ciss84@users.noreply.github.com>
2024-12-19 14:33:04 +0100

    Add files via upload

--- loader.js
+++ loader.js
@@ -5,7 +5,6 @@
 function Done() {
     document.getElementById("loader").style.display = "none";
     document.getElementById("alldone").style.display = "flex";
-    //document.getElementById("alldone").innerHTML = "Listening on Port: 9021";
 }
 function CalcTime(dur){hrs=Math.floor(dur/1000/60/60);min=Math.floor(dur/1000/60-hrs*60);sec=Math.floor(dur/1000-min*60);mil=dur.toString().slice(-3);if (min!=0){ShowDuration=" - WK Exploited In : "+min+" minute"+(min==1?"":"s")+", "+sec+" second"+(sec==1?"":"s");}else {ShowDuration=" - Exploited In: "+sec+" second"+(sec==1?"":"s");}}
 function StartTimer(){StartTime=Date.now();}
