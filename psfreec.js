71886387c175143c35de3280d30a790458355e08
Mugiwara <37186888+ciss84@users.noreply.github.com>
2024-12-19 13:53:44 +0100

    Add files via upload

--- psfreec.js
+++ psfreec.js
@@ -526,8 +526,8 @@ async function run() {
         }
     };       
     run_hax();
-    await new Promise((resolve) => setTimeout(resolve, 30000));
-    done();
+    //await new Promise((resolve) => setTimeout(resolve, 30000));
+    //done();
     } catch (error) {
         debug_log("[!] Webkit exploit failed: " + error);
         debug_log("[+] Retrying in 2 seconds...");
