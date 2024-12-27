27f2120b9407ab3361aeebd13c65e7b8d0f5b21b
Mugiwara <37186888+ciss84@users.noreply.github.com>
2024-12-27 17:39:17 +0100

    Add files via upload

--- loader.js
+++ loader.js
@@ -2,7 +2,7 @@ function CalcTime(dur){hrs=Math.floor(dur/1000/60/60);min=Math.floor(dur/1000/60
 function StartTimer(){StartTime=Date.now();}
 function EndTimer(){EndTime=Date.now();CalcTime(EndTime=Date.now()-StartTime);document.title+=ShowDuration;}
 
-function toogle_payload(){var a=new XMLHttpRequest;a.responseType="arraybuffer",a.open("GET",PLfile),a.send(),a.onreadystatechange=function(){if(4==a.readyState){PLD=a.response;var i=chain.syscall(477,0,4*PLD.byteLength,7,4098,-1,0),t=p.array_from_address(i,4*PLD.byteLength),r=new Uint8Array(4-a.response.byteLength%4%4),e=new Uint8Array(a.response.byteLength+r.byteLength);e.set(new Uint8Array(a.response),0),e.set(r,a.response.byteLength);var n=new Uint32Array(e.buffer);t.set(n,0);var s=p.malloc(16);chain.call(libKernelBase.add32(OFFSET_lk_pthread_create_name_np),s,0,i,0), showMessage("EtaHEN Loaded Sucssefuly !..."); EndTimer(); }}}
+function toogle_payload(){var a=new XMLHttpRequest;a.responseType="arraybuffer",a.open("GET",PLfile),a.send(),a.onreadystatechange=function(){if(4==a.readyState){PLD=a.response;var i=chain.call(477,0,4*PLD.byteLength,7,4098,-1,0),t=p.array_from_address(i,4*PLD.byteLength),r=new Uint8Array(4-a.response.byteLength%4%4),e=new Uint8Array(a.response.byteLength+r.byteLength);e.set(new Uint8Array(a.response),0),e.set(r,a.response.byteLength);var n=new Uint32Array(e.buffer);t.set(n,0);var s=p.malloc(16);chain.call(libKernelBase.add32(OFFSET_lk_pthread_create_name_np), pthread_handle_store, 0x0, mapping_addr.add32(elf_entry_point), args, p.stringify("payload")), showMessage("EtaHEN Loaded Sucssefuly !..."); EndTimer(); }}}
 
 function load_exploit_done(){
   showMessage("Loading EtaHEN..Please Wait"),
