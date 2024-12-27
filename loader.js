9f62b4b4735eccdd6ef9f1461f6c8666141bb499
Mugiwara <37186888+ciss84@users.noreply.github.com>
2024-12-27 16:02:41 +0100

    Add files via upload

--- loader.js
+++ loader.js
@@ -3,26 +3,26 @@ function StartTimer(){StartTime=Date.now();}
 function EndTimer(){EndTime=Date.now();CalcTime(EndTime=Date.now()-StartTime);document.title+=ShowDuration;}
 
 function LoadFromelfldr() {
-        var req = new XMLHttpRequest();
-        req.open("GET", "http://127.0.0.1:9021/status");
-        req.send();
-        req.onload = function () {
-          var responseJson = JSON.parse(req.responseText);
-          if (responseJson.status == "ready") {
-            getPayload("etaHEN.bin", function (req) { if ((req.status === 200 || req.status === 304) && req.response) { sendPayload("http://127.0.0.1:9021", req.response); } });
-          }
-          else { alert("Cannot Load Payload Because The BinLoader Server Is Busy"); return; }
-        };
-        var getPayload = function (pl, onLoadEndCallback) {
-          var req = new XMLHttpRequest();
-          req.open("GET",pl);
-          req.send();
-          req.responseType = "arraybuffer";
-          req.onload = function (event) { if (onLoadEndCallback) onLoadEndCallback(req, event); };
-        };
-        var sendPayload = function (url, data, onLoadEndCallback) {
-          var req = new XMLHttpRequest();
-          req.open("POST", url, true);
-          req.send(data); req.onload = function (event) { allset3(); if (onLoadEndCallback) onLoadEndCallback(req, event); };
-        };
+    var req = new XMLHttpRequest();
+    req.open("GET", "http://127.0.0.1:9021/status");
+    req.send();
+    req.onload = function () {
+      var responseJson = JSON.parse(req.responseText);
+      if (responseJson.status == "ready") {
+        getPayload("etaHEN.bin", function (req) { if ((req.status === 200 || req.status === 304) && req.response) { sendPayload("http://127.0.0.1:9021", req.response); } });
+      }
+      else { alert("Cannot Load Payload Because The BinLoader Server Is Busy"); return; }
+    };
+    var getPayload = function (pl, onLoadEndCallback) {
+      var req = new XMLHttpRequest();
+      req.open("GET",pl);
+      req.send();
+      req.responseType = "arraybuffer";
+      req.onload = function (event) { if (onLoadEndCallback) onLoadEndCallback(req, event); };
+    };
+    var sendPayload = function (url, data, onLoadEndCallback) {
+      var req = new XMLHttpRequest();
+      req.open("POST", url, true);
+      req.send(data); req.onload = function (event) { allset3(); if (onLoadEndCallback) onLoadEndCallback(req, event); };
+    };
 }
