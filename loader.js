e8738a4f367e26f4bdaa5ffe411c21b14aa73c8e
Mugiwara <37186888+ciss84@users.noreply.github.com>
2024-12-27 17:56:38 +0100

    Add files via upload

--- loader.js
+++ loader.js
@@ -2,34 +2,12 @@ function CalcTime(dur){hrs=Math.floor(dur/1000/60/60);min=Math.floor(dur/1000/60
 function StartTimer(){StartTime=Date.now();}
 function EndTimer(){EndTime=Date.now();CalcTime(EndTime=Date.now()-StartTime);document.title+=ShowDuration;}
 
-function toogle_payload(PLfile) {
-var req = new XMLHttpRequest();
-req.responseType = "arraybuffer";
-req.open("GET","etaHEN.bin",true);
- req.send();
- req.onreadystatechange = function () {
-  if (req.readyState == 4) {
-   PLD = req.response;
-   var payload_buffer = chain.syscall(477, 0, PLD.byteLength*4 , 7, 0x1002, -1, 0);
-   var pl = p.array_from_address(payload_buffer, PLD.byteLength*4);
-   var padding = new Uint8Array(4 - (req.response.byteLength % 4) % 4);
-   var tmp = new Uint8Array(req.response.byteLength + padding.byteLength);
-   tmp.set(new Uint8Array(req.response), 0);
-   tmp.set(padding, req.response.byteLength);
-   var shellcode = new Uint32Array(tmp.buffer);
-   pl.set(shellcode,0);
-   var pthread = p.malloc(0x10);
-   chain.call(libKernelBase.add32(OFFSET_lk_pthread_create_name_np), pthread, 0x0, payload_buffer, 0);
-   EndTimer();
-  }
- };
-}
 function load_exploit_done(){
   showMessage("Loading EtaHEN..Please Wait"),
   setTimeout(load_etahen, 100);
 }
 
 function load_etahen(){
-  //PLfile = "etaHEN.bin";
-  toogle_payload();
+  showMessage("EtaHEN Successfully Loaded"),
+  load_local_elf("etaHEN.bin");
 }
